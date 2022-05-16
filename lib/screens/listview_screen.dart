1ccd7654d1c6ee4afe496bd639fcc3b21b6c82fd
francmarin98 <francmarinc@gmail.com>
2022-05-16 17:53:59 -0500

    Learning about ListView, ListTable and create nuew pages

---
+++ lib/screens/listview_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ListViewScreenOne extends StatelessWidget {
+  final options = const ['One', 'Two', 'Three', 'Four', 'Five', 'Six'];
+
+  const ListViewScreenOne({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("ListViewScreenOne")),
+      body: ListView(
+        children: [
+          ...options
+              .map((numero) => ListTile(
+                    title: Text(numero),
+                    trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                  ))
+              .toList()
+        ],
+      ),
+    );
+  }
+}
